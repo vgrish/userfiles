9b3c192d2c3006a4966fd9b9d4939c27eeaf42b6
vovanblya <vovanblya@mail.ru>
2016-03-18 16:52:54 +0300

    2.1.1-beta (18.03.2016) ============== - Fix resource tab (AjaxManager)

--- assets/components/userfiles/js/mgr/inject/resource.tab.js
+++ assets/components/userfiles/js/mgr/inject/resource.tab.js
@@ -9,8 +9,18 @@ Ext.override(MODx.panel.Resource, {
     getFields: function(config) {
         var fields = this.originals.getFields.call(this, config);
 
+        var isHide = false;
+        var templates = MODx.config.userfiles_working_templates || '';
+        if (templates.split(',').indexOf(''+config.record.template+'') < 0) {
+            isHide = true;
+        }
+
+        if (!userfiles.config.resource) {
+            isHide = true;
+        }
+
         var tabs = fields.filter(function(row) {
-            if (row.id == 'modx-resource-tabs') {
+            if (row.id == 'modx-resource-tabs' && !isHide) {
                 row.items.push({
                     xtype: 'userfiles-panel-resource-file',
                     title: _('userfiles')
@@ -29,6 +39,7 @@ Ext.ComponentMgr.onAvailable('modx-resource-tabs', function() {
 
     var tabs = this;
     tabs.on('beforerender', function() {
+
         var is = tabs.items.items.filter(function(row) {
             if (row.id == 'userfiles-panel-resource') {
                 return true;
@@ -37,7 +48,19 @@ Ext.ComponentMgr.onAvailable('modx-resource-tabs', function() {
             }
         });
 
-        if (is.length == 0) {
+        var isHide = false;
+
+        var $resource = Ext.getCmp("modx-page-update-resource");
+        var templates = MODx.config.userfiles_working_templates || '';
+        if (templates.split(',').indexOf(''+$resource.record.template+'') < 0) {
+            isHide = true;
+        }
+
+        if (!userfiles.config.resource) {
+            isHide = true;
+        }
+
+        if (is.length == 0 && !isHide) {
             tabs.add({
                 xtype: 'userfiles-panel-resource-file',
                 title: _('userfiles')
